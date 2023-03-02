6ce25e36f9a54d7ae4ab814d66b67c024f252b4f
Mahmoud Ibrahim <xmanali92@gmail.com>
2023-03-02 18:11:09 +0200

    Add files via upload

---
+++ js/swiper-config.js
@@ -0,0 +1,20 @@
+  import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+
+  const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      loop: true,
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+          el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      modules: [Navigation, Pagination],
+
+  });
